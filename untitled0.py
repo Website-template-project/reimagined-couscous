26b427eea6823af7b6af698db703c0a6001d4368
khoa <khoa1999@github.com>
2024-07-13 22:37:51 +0700

    AI config

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 18:51:46 2024
+
+@author: dangkhoa
+"""
+
+import vertexai
+from vertexai.generative_models import GenerativeModel

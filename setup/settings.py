6e92dfbb96ae3678f2e121cc1a4358988f56790a
khoa1999 <43160084+khoa1999@users.noreply.github.com>
2025-06-17 15:03:19 +0700

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -270,7 +270,7 @@ else:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
 # Optional: if you want to use caching for session data
